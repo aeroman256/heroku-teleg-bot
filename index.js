e11793d5d2b460be92dff98aaf256dd0ac1b1a9a
aeroman256 <zakharov.dmitry.92@yandex.ru>
2019-04-05 17:30:17 +0300

    change tk

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-const TOKEN = 's...U...L...Q...6..._...h...U...H...V...G...9...u...s...l...x...k...g...y...4...f...p...Z...2...M...y...y...R...L...Y...P...0...o...E...A...A...:...9...9...8...5...8...3...1...3...7';
+const TOKEN = '731385899:AAEo0PYLRyyM2Zpf4ygkxlsu9GVHUh_6QLUs';
 let untkn = TOKEN.split('').reverse().filter(function(key) {if(key !=="."){return key}}).join('');
 const bot = new TelegramBot(untkn, {polling: true});
 
