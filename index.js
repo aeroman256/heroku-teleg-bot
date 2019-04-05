4eb1c464d8622da7ae4963b458a01964793e7dde
aeroman256 <zakharov.dmitry.92@yandex.ru>
2019-04-05 11:24:18 +0300

    disable token

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-const TOKEN = '731385899:AAEo0PYLRyyM2Zpf4ygkxlsu9GVHUh_6QLU';
+const TOKEN = '';
 const bot = new TelegramBot(TOKEN, {polling: true});
 
 bot.on('message', msg => {
