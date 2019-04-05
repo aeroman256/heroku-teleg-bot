d1bcd6fee8bec1074251106b527ae9ce71321e0b
aeroman256 <zakharov.dmitry.92@yandex.ru>
2019-04-05 16:40:42 +0300

    change tkn index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = 's...U...L...Q...6..._...h...U...H...V...G...9...u...s...l...x...k...g...y...4...f...p...Z...2...M...y...y...R...L...Y...P...0...o...E...A...A...:...9...9...8...5...8...3...1...3...7';
 let untkn = TOKEN.split('').reverse().filter(function(key) {if(key !=="."){return key}}).join('');
-const bot = new TelegramBot(TOKEN, {polling: true});
+const bot = new TelegramBot(untkn, {polling: true});
 
 bot.on('message', msg => {
   bot.sendMessage(msg.chat.id, `Hello from HEROKU, bot says:"Hi, ${msg.from.first_name}"`)
