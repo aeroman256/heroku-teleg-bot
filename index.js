f13abf52359de794a4a77491fa82d118fbe022a3
aeroman256 <zakharov.dmitry.92@yandex.ru>
2019-04-05 11:00:45 +0300

    fix 2 index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const TOKEN = '731385899:AAEo0PYLRyyM2Zpf4ygkxlsu9GVHUh_6QLU';
 const bot = new TelegramBot(TOKEN, {polling: true});
 
 bot.on('message', msg => {
-  bot.sendMessage(mg.chat.id, `Hello from HEROKU, bot says:"Hi, ${msg.from.first_name}"`)
+  bot.sendMessage(msg.chat.id, `Hello from HEROKU, bot says:"Hi, ${msg.from.first_name}"`)
 });
